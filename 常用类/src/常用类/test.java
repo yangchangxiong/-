19499dc63260b1c563aa61b9849e9fce43b8dd97
yangchangxiong <2426606443@qq.com>
2019-06-11 01:03:26 +0800

    完成

--- 常用类/src/常用类/test.java
+++ 常用类/src/常用类/test.java
@@ -1,15 +1,48 @@
 package 常用类;
 
+import java.util.Arrays;
+
 public class test {
 private static String str="this is a test of java";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		test1();
 		test2();
-		//test3();
-		//test4();
-		//test5();
+		test3();
+		test4();
+		test5();
 
+	}
+	private static void test5() {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse().toString());
+
+		StringBuffer sb2 = new StringBuffer();
+		for (int i = str.length()-1; i >=0; i--) {
+		sb.append(str.charAt(i));}
+		System.out.println("--"+sb2);
+	}
+	
+	private static void test4() {
+		// TODO Auto-generated method stub
+		String[] ss = str.split(" ");//其実用空格并不准确，应该使用正则表达式
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < ss.length; i++) {
+		String s = ss[i].substring(0,1).toUpperCase()+ss[i].substring(1);
+		sb.append(s);}
+		System.out.println(sb);
+	}
+	private static void test3() {
+		// TODO Auto-generated method stub
+		char[] ch = new char[str.length()];
+		for (int i = 0; i < str.length(); i++) {
+		ch[i] = str.charAt(i);}
+
+	System.out.println(Arrays.toString(ch));
+
+	char[] ch2 = str.toCharArray();
+	System.out.println(Arrays.toString(ch2));
 	}
 	private static void test2() {
 		// TODO Auto-generated method stub
@@ -26,4 +59,5 @@ private static String str="this is a test of java";
 		}
 	}
 		System.out.println("s出现的次数"+count);
+	}
 	}
